55ad50d4aeaaf5e42f0e018264a48c39356bce61
memorieslp <42478443+memorieslp@users.noreply.github.com>
2021-11-21 17:57:45 -0300

    Added and configured eslint to back and front ends (#5)

--- back/index.js
+++ back/index.js
@@ -1,42 +1,45 @@
 const service = require('./service');
 
-const requestManager = response => {
+const requestManager = (response) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({content: response})
-  }
-}
+    body: JSON.stringify({ content: response }),
+  };
+};
 
 const fnHandler = async (context, event) => {
   try {
-    const func = FUNCTION_PATH[event.rawPath]
-    return await func(event)
-  }
-  catch (ex) {
+    const func = FUNCTION_PATH[event.rawPath];
+    return await func(event);
+  } catch (ex) {
     return {
       statusCode: 400,
       body: JSON.stringify({
-            message: `Bad request ${ex}`,
+        message: `Bad request ${ex}`,
       }),
-    }
+    };
   }
-}
+};
 
 const createKudoHandler = (event) => {
-  const kudoData = JSON.parse(event.body)
-  return Promise.resolve(service.kudoService().createKudo(kudoData)).then(response => requestManager(JSON.parse(event.body)))
-}
+  const kudoData = JSON.parse(event.body);
+  return Promise.resolve(service.kudoService().createKudo(kudoData)).then(() =>
+    requestManager(JSON.parse(event.body))
+  );
+};
 
-const getKudos = async (event) => {
-  return Promise.resolve(service.kudoService().getKudos()).then(response => requestManager(response))
-}
+const getKudos = async () => {
+  return Promise.resolve(service.kudoService().getKudos()).then((response) =>
+    requestManager(response)
+  );
+};
 
 const FUNCTION_PATH = {
-  '/kudo':  createKudoHandler,
-  '/kudos':  getKudos
-}
+  '/kudo': createKudoHandler,
+  '/kudos': getKudos,
+};
 
 module.exports.handler = async (event, context, callback) => {
   const response = await fnHandler(context, event);
-  callback(null, response)
+  callback(null, response);
 };
