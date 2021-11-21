a21ea9a7ec8860dcfc3e56df5c5351f99912f5a5
memorieslp <42478443+memorieslp@users.noreply.github.com>
2021-11-21 18:10:37 -0300

    Adding ESLint to front end - I did not include it on previous commit by mistake

--- front/src/components/composer/Composer.jsx
+++ front/src/components/composer/Composer.jsx
@@ -1,32 +1,34 @@
 import { useState } from 'react';
 import styles from './Composer.module.css';
 
-
 export default function Composer() {
-    const [kudoContent, setKudoContent] = useState('')
-    
-    function handleSubmit(event) {
-        event.preventDefault()
-        console.log(kudoContent)
-    }
+  const [kudoContent, setKudoContent] = useState('');
 
-    function handleChange(event) {
-        setKudoContent(event.target.value)
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(kudoContent);
+  }
 
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.avatar} />
-            <form className={styles.composer} onSubmit={handleSubmit}>
-                <input className={styles.input} type="text" placeholder="Kudo para..." />
-                <textarea 
-                    className={styles.textarea}
-                    cols={25} 
-                    rows={5} 
-                    placeholder="Escreva seu kudo..." 
-                    onChange={handleChange} />
-                <button className={styles.composerButton} type="submit">Publicar!</button>
-            </form>
-        </div>
-    ); 
+  function handleChange(event) {
+    setKudoContent(event.target.value);
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.avatar} />
+      <form className={styles.composer} onSubmit={handleSubmit}>
+        <input className={styles.input} type="text" placeholder="Kudo para..." />
+        <textarea
+          className={styles.textarea}
+          cols={25}
+          rows={5}
+          placeholder="Escreva seu kudo..."
+          onChange={handleChange}
+        />
+        <button className={styles.composerButton} type="submit">
+          Publicar!
+        </button>
+      </form>
+    </div>
+  );
 }
