60b0eab10d88d8dbc46ce15d772bab7221663ff0
Rafael Roth <rbiicalho@gmail.com>
2021-11-21 07:13:32 -0300

    adding post and get endpoints

---
+++ back/service/index.js
@@ -0,0 +1,13 @@
+const repository = require('../repository')
+
+module.exports.kudoService = () => {
+    return {
+        createKudo: (kudo) => {
+            return repository.kudosRepository().insertKudo(kudo)
+        },
+        getKudos: async () => {
+            const data = await repository.kudosRepository().getKudos()
+            return data
+        }
+    }
+}
