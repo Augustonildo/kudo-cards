75035fc3dcf6205b968604b049492d85e037b582
memorieslp <42478443+memorieslp@users.noreply.github.com>
2021-11-21 16:32:55 -0300

    Added and configured eslint to back and front ends

--- back/repository/index.js
+++ back/repository/index.js
@@ -1,45 +1,46 @@
-const AWS = require("aws-sdk")
-const uuid = require("uuid")
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
 
 module.exports.kudosRepository = () => {
-  const dynamoDB = new AWS.DynamoDB.DocumentClient({ region: "us-east-1" })
-  const TableName = 'kudos'
+  const dynamoDB = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
+  const TableName = 'kudos';
   return {
-    insertKudo: kudo => {
+    insertKudo: (kudo) => {
       const params = {
         TableName,
         Item: {
           id: uuid.v4(),
           date: new Date().toISOString(),
-          ...kudo
-        }
-      }
-      try{
-        dynamoDB.put(params, function(err, data) {
+          ...kudo,
+        },
+      };
+      try {
+        dynamoDB.put(params, function (err, data) {
           if (err) {
-              throw("Unable to add kudo. Error JSON:", JSON.stringify(err, null, 2))
+            throw ('Unable to add kudo. Error JSON:', JSON.stringify(err, null, 2));
           } else {
-              return `Added kudo:" ${JSON.stringify(data, null, 2)}`
+            return `Added kudo:" ${JSON.stringify(data, null, 2)}`;
           }
         });
-      }
-      catch (err) {
-        console.log(err)
+      } catch (err) {
+        console.log(err);
       }
     },
     getKudos: async () => {
       const params = {
         TableName,
-        Select: "ALL_ATTRIBUTES"
-      }
-      const data = await dynamoDB.scan(params, function(err, data) {
-        if (err) {
-            throw("Unable to query. Error:", JSON.stringify(err, null, 2))
-        } else {
-            console.log(`Query succeeded, ${data.Items.length} returned.`)
+        Select: 'ALL_ATTRIBUTES',
+      };
+      const data = await dynamoDB
+        .scan(params, function (err, data) {
+          if (err) {
+            throw ('Unable to query. Error:', JSON.stringify(err, null, 2));
+          } else {
+            console.log(`Query succeeded, ${data.Items.length} returned.`);
           }
-      }).promise()
-      return data.Items
-    }
-  }
-}
+        })
+        .promise();
+      return data.Items;
+    },
+  };
+};
--- back/service/index.js
+++ back/service/index.js
@@ -1,13 +1,13 @@
-const repository = require('../repository')
+const repository = require('../repository');
 
 module.exports.kudoService = () => {
-    return {
-        createKudo: (kudo) => {
-            return repository.kudosRepository().insertKudo(kudo)
-        },
-        getKudos: async () => {
-            const data = await repository.kudosRepository().getKudos()
-            return data
-        }
-    }
-}
+  return {
+    createKudo: (kudo) => {
+      return repository.kudosRepository().insertKudo(kudo);
+    },
+    getKudos: async () => {
+      const data = await repository.kudosRepository().getKudos();
+      return data;
+    },
+  };
+};
