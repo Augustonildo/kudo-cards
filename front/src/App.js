41aeb807c9035442f927fcdab2eb12bf74735d7b
augustonildo <augustocarvalhopp@gmail.com>
2022-01-08 17:16:08 -0300

    Roteamento de páginas do sistema

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,19 @@
-import Timeline from './components/timeline/Timeline';
-import KudoProvider from './contexts/KudoContext';
+import { Outlet, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Profile from './pages/profile/Profile';
 
 function App() {
   return (
-    <main>
-      <KudoProvider>
-        <Timeline />
-      </KudoProvider>
-    </main>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Outlet />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
