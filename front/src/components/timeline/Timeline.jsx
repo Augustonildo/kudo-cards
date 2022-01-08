876241dd87de43fd2533d22f5281fb59cc7315b3
GuilhermeLLS <guiluizls@gmail.com>
2022-01-08 15:30:51 -0300

    refactor: initial changes to <Timeline />

--- front/src/components/timeline/Timeline.jsx
+++ front/src/components/timeline/Timeline.jsx
@@ -1,26 +1,39 @@
-import { useContext } from 'react';
+import { Fragment, useContext } from 'react';
 import { KudoContext } from '../../contexts/KudoContext';
 import Card from '../card/Card';
 import Composer from '../composer/Composer';
+import Logo from '../logo/Logo';
+import Sidebar from '../sidebar/Sidebar';
 import styles from './Timeline.module.css';
 
 export default function Timeline() {
   const { kudos } = useContext(KudoContext);
 
   return (
-    <div className={styles.timeline}>
-      <div className={styles.logo}>
-        <h1>Kudo Cards!</h1>
+    <Fragment>
+      <Sidebar />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          margin: 'auto',
+          justifyContent: 'center',
+          height: '100vh',
+        }}
+      >
+        <div className={styles.timeline}>
+          <Logo size="md" />
+          <Composer />
+          {kudos.map((kudo) => (
+            <Card
+              key={kudo.id}
+              sender={kudo.sender}
+              recipient={kudo.recipient}
+              message={kudo.message}
+            />
+          ))}
+        </div>
       </div>
-      <Composer />
-      {kudos.map((kudo) => (
-        <Card
-          key={kudo.id}
-          sender={kudo.sender}
-          recipient={kudo.recipient}
-          message={kudo.message}
-        />
-      ))}
-    </div>
+    </Fragment>
   );
 }
