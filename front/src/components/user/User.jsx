8a2db195407b99c7bc52be50f8f1a2a69935b74f
augustonildo <augustocarvalhopp@gmail.com>
2022-01-08 19:57:53 -0300

    feat: username and logo on profile

--- front/src/components/user/User.jsx
+++ front/src/components/user/User.jsx
@@ -1,5 +1,6 @@
 import { FiArrowLeft } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import Avatar from '../avatar/Avatar';
 import styles from './User.module.css';
 
 export default function User() {
@@ -11,10 +12,15 @@ export default function User() {
             <span className={styles.returnButtonIcon}>
               <FiArrowLeft />
             </span>
-            <span>Perfil</span>
+            <span>Voltar</span>
           </Link>
         </div>
       </div>
+      {/* TODO: Alterar "Nome Usuário Teste" para exibir nome do usuário logado */}
+      <div className={styles.userImage}>
+        <Avatar name="Nome Usuário Teste" size="104" radius="50" />
+      </div>
+      <p className={styles.username}> Nome Usuário Teste </p>
     </div>
   );
 }
