8eec9ef75934db36bb94b89fcfa7bf7f5aa1c53b
Guilherme Luiz <guiluizls@gmail.com>
2022-01-09 18:37:03 -0300

    fix: add context provider to authenticated routes (#19)

--- front/src/pages/index/AuthenticatedApp.jsx
+++ front/src/pages/index/AuthenticatedApp.jsx
@@ -1,15 +1,18 @@
 import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
+import KudoProvider from '../../contexts/KudoContext';
 import Home from '../home/Home';
 import Profile from '../profile/Profile';
 
 export default function AuthenticatedApp() {
   return (
-    <Routes>
-      <Route path="/" element={<Outlet />}>
-        <Route index element={<Home />} />
-        <Route path="profile" element={<Profile />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <KudoProvider>
+      <Routes>
+        <Route path="/" element={<Outlet />}>
+          <Route index element={<Home />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </KudoProvider>
   );
 }
