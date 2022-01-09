7c3a6b40a189ebea6cfb0e89b554791b5d3bbdf6
Augusto Carvalho <augustocarvalhopp@gmail.com>
2022-01-09 09:35:45 -0300

    feat: user profile (#12)
    * feat: redirect in/out profile
    
    * feat: username and logo on profile
    
    * fix: add Avatar prop types

--- front/src/components/avatar/Avatar.jsx
+++ front/src/components/avatar/Avatar.jsx
@@ -2,23 +2,25 @@ import PropTypes from 'prop-types';
 import { createAvatar } from '@dicebear/avatars';
 import * as style from '@dicebear/avatars-initials-sprites';
 
-function createAvatarFromSeed(seed) {
+function createAvatarFromSeed(seed, size, radius) {
   return createAvatar(style, {
     seed,
-    size: 40,
-    radius: 20,
+    size: size,
+    radius: radius,
   });
 }
 
-export default function Avatar({ name }) {
+export default function Avatar({ name, size = 40, radius = 20 }) {
   return (
     <span
       style={{ width: 'min-content' }}
-      dangerouslySetInnerHTML={{ __html: createAvatarFromSeed(name) }}
+      dangerouslySetInnerHTML={{ __html: createAvatarFromSeed(name, size, radius) }}
     ></span>
   );
 }
 
 Avatar.propTypes = {
   name: PropTypes.string,
+  size: PropTypes.number,
+  radius: PropTypes.number,
 };
---
+++ front/src/components/user/User.jsx
@@ -0,0 +1,26 @@
+import { FiArrowLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import Avatar from '../avatar/Avatar';
+import styles from './User.module.css';
+
+export default function User() {
+  return (
+    <div className={styles.user}>
+      <div className={styles.profileBackground}>
+        <div>
+          <Link to="/" className={styles.returnButton}>
+            <span className={styles.returnButtonIcon}>
+              <FiArrowLeft />
+            </span>
+            <span>Voltar</span>
+          </Link>
+        </div>
+      </div>
+      {/* TODO: Alterar "Nome Usuário Teste" para exibir nome do usuário logado */}
+      <div className={styles.userImage}>
+        <Avatar name="Nome Usuário Teste" size="104" radius="50" />
+      </div>
+      <p className={styles.username}> Nome Usuário Teste </p>
+    </div>
+  );
+}
