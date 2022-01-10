5ce5c8f65444aaf1c88f192a52c0a62a0f3179ed
augustonildo <augustocarvalhopp@gmail.com>
2022-01-10 12:24:29 -0300

    refactor: sort function place

--- back/src/kudos/service.js
+++ back/src/kudos/service.js
@@ -1,12 +1,12 @@
 const repository = require('./repository');
 
-const sortObjectArrayByDate = (a, b) => {
-  if (a.date < b.date) return 1;
-  if (a.date > b.date) return -1;
-  return 0;
-};
-
 module.exports.kudoService = () => {
+  const sortObjectArrayByDate = (a, b) => {
+    if (a.date < b.date) return 1;
+    if (a.date > b.date) return -1;
+    return 0;
+  };
+
   return {
     createKudo: (kudo) => {
       return repository.kudosRepository().insertKudo(kudo);
