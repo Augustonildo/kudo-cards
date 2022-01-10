c248b621794d80ffee62a76fa74278e10c952547
Rafael Roth <rbiicalho@gmail.com>
2022-01-10 14:59:18 -0300

    test: parse to aws format

--- back/__tests__/users/service.test.js
+++ back/__tests__/users/service.test.js
@@ -5,4 +5,16 @@ describe('kudos service', () => {
         const successfullyLoginResponse = userService.succesfullyLogin();
         expect(successfullyLoginResponse.message).toBe('Login Successfully');
     });
+    it('Should parse data to aws format', () => {
+        const user =  {
+            name: 'Teste',
+            email: 'teste@email.com',
+            password: 'teste senha'
+        };
+        const awsUserFormat = userService.parseUserDataToAwsFormat(user)
+        expect(awsUserFormat.Username).toBe(user.email);
+        expect(awsUserFormat.Password).toBe(user.password);
+        expect(awsUserFormat.UserAttributes[0].Name).toBe('name');
+        expect(awsUserFormat.UserAttributes[0].Value).toBe(user.name);
+    });
 })
