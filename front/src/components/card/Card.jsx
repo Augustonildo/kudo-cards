c53473af7313d3571b8738e2f26b7a35a15b069a
augustonildo <augustocarvalhopp@gmail.com>
2022-01-09 21:29:02 -0300

    fix: remove reactions (for now)

--- front/src/components/card/Card.jsx
+++ front/src/components/card/Card.jsx
@@ -1,31 +1,31 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Avatar from '../avatar/Avatar';
 import styles from './Card.module.css';
 
 export default function Card({ recipient, sender, message }) {
-  const [reaction, setReaction] = useState();
-  const [openReactionBox, setOpenReactionBox] = useState();
+  // const [reaction, setReaction] = useState();
+  // const [openReactionBox, setOpenReactionBox] = useState();
 
   return (
     <article className={styles.card}>
       <Avatar name={sender.label} />
       <div className={styles.cWrapper}>
         <div className={styles.header}>
-          <span>
+          <span className={styles.cardTitle}>
             {sender.label}
             <br />
-            Para {recipient.label}
+            Para: {recipient.label}
           </span>
         </div>
         <span className={styles.content}>{message}</span>
-        <div onClick={() => setOpenReactionBox((prevState) => !prevState)}>Reações:</div>
+        {/* <div onClick={() => setOpenReactionBox((prevState) => !prevState)}>Reações:</div>
         <div>{reaction}</div>
         <div>
           {openReactionBox ? (
             <input type="text" onChange={(e) => setReaction(e.target.value)} />
           ) : null}
-        </div>
+        </div> */}
       </div>
     </article>
   );
